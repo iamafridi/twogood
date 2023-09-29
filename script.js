83d420d492e46a85a65ad194b6132d975187343d
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-09-29 11:54:44 +0600

    Locomotive added

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+
 function videoconAnimation(){
     var videocon = document.querySelector("#video-container")
 var playbtn = document.querySelector("#play")
